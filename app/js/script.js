ce27a2d3fea02a52c8909bccd2f5349e6ff20d5d
Mateusz <108770502+musior@users.noreply.github.com>
2023-11-29 20:04:52 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,43 @@
-console.log("Test message");
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.querySelector('body');
+
+const setDarkMode = () => {
+  body.classList = 'dark';
+  localStorage.setItem('colorMode', 'dark');
+}
+
+const setLightMode = () => {
+  body.classList = 'light';
+  localStorage.setItem('colorMode', 'light');
+}
+
+const colorModeFromLocalStorage = () => {
+  return localStorage.getItem('colorMode');
+}
+
+const colorModeFromPreferences = () => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
+
+const loadAndUpdateColor = () => {
+  const color = colorModeFromLocalStorage() || colorModeFromPreferences();
+  color == 'dark' ? darkButton.click() : lightButton.click();
+}
+
+for (let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].addEventListener('click', event => {
+    if (darkButton.checked) {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
+  })
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', (event) => {
+  event.matches ? darkButton.click() : lightButton.click();
+});
+
+loadAndUpdateColor();
